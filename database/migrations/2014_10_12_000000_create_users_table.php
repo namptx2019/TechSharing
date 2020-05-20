428ac45a8cba1b53715c921262cfc9bdc98803c6
Dai Minh <1653050@student.hcmus.edu.vn>
2020-05-20 15:22:04 +0700

    init(): First init source

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->increments('id');
+            $table->string('username', 255);
+            $table->string('email', 255);
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('gender');
+            $table->string('phone', 255)->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->text('working_place')->nullable();
             $table->timestamps();
         });
     }
