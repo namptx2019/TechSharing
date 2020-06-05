4de4dff6c86d8d7ad81d393fa9f4645880fee9ce
Nam Phan <namptx2019@gmail.com>
2020-06-06 00:47:26 +0700

    Navigate

--- frontend/homepage/src/MainNav.js
+++ frontend/homepage/src/MainNav.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
 import './index.css';
+import Login from './Login/Login';
 
 const MainNav = () => {
     return(
@@ -8,47 +15,49 @@ const MainNav = () => {
 			<div className="container">
 				<div className="header-section-wrapper d-flex align-items-stretch justify-content-between">
 
-					<div classNameName="logo">
-						<img src = {require('./static/images/logo/black.svg')}/>
-					</div>
+					<Link to ="/" className="logo">
+						<img src = {require('./static/images/logo/black.png')}/>
+					</Link>
 
-					<div className="nav-items d-none d-xl-block">
-						<div className="nav-items-wrapper">
-							<ul className="d-flex justify-content-end align-items-center no-padding no-margin">
-								<li>
-									<div className="nav-item category-dropdown" style={{color:'black'}}>
-										<div className="nav-item-wrapper">
-											<span className="category-dropdown-text">Categories <i className="icon-enter-arrow-down"></i></span>
-											<transition name="fade">
-												<categories-header v-show="showCategory"/>
-											</transition>
+					
+						<div className="nav-items d-none d-xl-block">
+							<div className="nav-items-wrapper">
+								<ul className="d-flex justify-content-end align-items-center no-padding no-margin">
+									<li>
+										<div className="nav-item category-dropdown" style={{color:'black'}}>
+											<div className="nav-item-wrapper">
+												<span className="category-dropdown-text">Categories <i className="icon-enter-arrow-down"></i></span>
+												<transition name="fade">
+													<categories-header v-show="showCategory"/>
+												</transition>
+											</div>
 										</div>
-									</div>
-								</li>
-								<li>
-									<div className="nav-item" style={{color:'black'}}>Series</div>
-								</li>
-								<li>
-									<div className="nav-item" style={{color:'black'}}>Posts</div>
-								</li>
-								<li>
-									<div className="nav-item" style={{color:'black'}}>Contests</div>
-								</li>
-								<li>
-									<div className="nav-item" style={{color:'black'}}>Test and Get Hired</div>
-								</li>
+									</li>
+									<li>
+										<Link className="nav-item" style={{color:'black'}}>Series</Link>
+									</li>
+									<li>
+										<Link className="nav-item" style={{color:'black'}}>Posts</Link>
+									</li>
+									<li>
+										<Link className="nav-item" style={{color:'black'}}>Contests</Link>
+									</li>
+									<li>
+										<Link className="nav-item" style={{color:'black'}}>Test and Get Hired</Link>
+									</li>
 
-								<li>
-									<button className="btn nav-button btn-danger">Login</button>
-								</li>
-								
-							</ul>
+									<li>
+										<Link to ="/login">
+											<button className="btn nav-button btn-danger">Login</button>
+										</Link>
+									</li>
+									
+								</ul>
+							</div>
 						</div>
-					</div>
 				</div>
 			</div>
 		</div>
-        
     );
 }
 
--- frontend/homepage/src/index.js
+++ frontend/homepage/src/index.js
@@ -3,12 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './Login/Login';
 import MainNav from './MainNav';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams,
+    useRouteMatch
+  } from "react-router-dom";
+import App from './App';
 
 function Homepage () {
     return(
         <div>
-            <MainNav/>
-            <Login/>
+            <Router>
+                <MainNav/>
+
+                <Switch>
+                    <Route exact path="/">
+                        <App />
+                    </Route>
+
+                    <Route exact path="/login">
+                        <Login />
+                    </Route>
+                </Switch>
+            </Router>
+            
         </div>
     );
 }
