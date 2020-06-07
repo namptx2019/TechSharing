237c2cf1fb1e346a4c24e0f9e992d3e824762d3d
Nam Phan <namptx2019@gmail.com>
2020-06-07 09:38:47 +0700

    latest post, leader boards, Series

--- frontend/homepage/src/Slider/Slider.js
+++ frontend/homepage/src/Slider/Slider.js
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import '../index.css';
+import 'swiper/css/swiper.css';
 import Swiper from 'react-id-swiper';
 
 const Slider = () => {
     const params = {
         pagination: {
           el: '.swiper-pagination',
-          type: 'progressbar',
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          clickable: true,
+          dynamicBullets: true
         }
-    }
+      }
 
     return(
-        <div>
+        <div style={{marginBottom: '20px'}}>
             <section className="slider-block" v-if="this.sliders != null">
                 <div className="container">
                     <div className="slider">
@@ -47,9 +45,31 @@ const Slider = () => {
                                     </div>
                                 </a>
                             </div>
+
+                            <div className="item">
+                                <a target="_blank">
+                                    <div className="row">
+                                        <div className="col-12 d-sm-none d-md-none">
+                                            <div className="banner">
+                                                <div className="img" style={{backgroundSize: '100%'}}></div>
+                                            </div>
+                                        </div>
+                                        <div className="col-12 col-sm-7">
+                                            <div className="content">
+                                                <h1 className="heading">Title</h1>
+                                                <p className="desc">Description will be here!</p>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-5 d-none d-sm-block">
+                                            <div className="banner">
+                                                <div className="img"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
                                 
                         </Swiper>
-                        <div className="swiper-pagination custom-pagination" slot="pagination"></div>
                     </div>
                 </div>
             </section>
