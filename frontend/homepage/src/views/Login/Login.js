7726af2e0ef858683678c29c07f228ae85083ada
Minh <thdaiminh1998@gmail.com>
2020-06-08 16:34:27 +0700

    feat(homepage/Login): + add thumbnail for login for,

--- frontend/homepage/src/views/Login/Login.js
+++ frontend/homepage/src/views/Login/Login.js
@@ -3,6 +3,7 @@ import {
     Alert
 } from 'reactstrap';
 import UserService, { UserServiceError } from "../../services/user.service"
+import DEFAULT_BG from "../../static/images/thumbnail/3.png"
 
 const Login = () => {
     const [User, setUser] = useState({});
@@ -21,7 +22,7 @@ const Login = () => {
         }
     };
     return(
-        <section className="section-block">
+        <section className="section-block page-auth">
             <div className="container">
                 <div className="row">
                     { error.length !== 0 &&
@@ -37,6 +38,11 @@ const Login = () => {
                             <input placeholder="Email" type="email" name="email" className="form-control" onChange={(e)=> handleChange(e)}/>
                             <input placeholder="Password" type="password" name="password" className="form-control" onChange={(e)=> handleChange(e)}/>
                             <button type="button" className="btn btn-primary" onClick={() => handleSubmit(User)}>Login</button>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 d-none d-lg-block">
+                        <div className="right-bg">
+                            <img src={DEFAULT_BG}/>
                         </div>
                     </div>
                 </div>
